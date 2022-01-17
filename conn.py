de80d1b594877dec5af75c3f16e30c2436e31e7c
Robson Ferreira <tech.rob@icloud.com>
2022-01-17 08:08:24 -0500

    updates

---
+++ conn.py
@@ -0,0 +1,19 @@
+def connect():
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="mysqlpassmacrob",
+            database="employees"
+        )
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        conn.close()
+
+connect()
